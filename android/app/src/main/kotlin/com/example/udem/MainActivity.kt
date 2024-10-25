6567e09dd2006849a70a46fced1951c56600c151
Jesus Manuel <jmlr231201@gmail.com>
2024-10-25 13:18:23 -0600

    Empezando curso

---
+++ android/app/src/main/kotlin/com/example/udem/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.udem
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Center(
+    child: Text("Hola flutter",
+    textDirection: TextDirection.ltr,
+    ),
+  ));
+}
