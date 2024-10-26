5cc487be3313d27a9f203b0b56886fbebcd3a741
Jesus Manuel <jmlr231201@gmail.com>
2024-10-26 10:09:39 -0600

    First practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:css_colors/css_colors.dart';
 
 void main() {
   runApp(Center(
-    child: Text("Hola flutter",
-    textDirection: TextDirection.ltr,
+    child: Text(
+      "Hola flutter",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(
+        color: CSSColors.lightBlue, // Color rosa
+      ),
     ),
   ));
 }
